7f33d4728e27da8ebb6eea2743eb82397caa492c
KagChi <yonuxbot@gmail.com>
2022-07-01 18:40:51 +0700

    chore: bump deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import { AppProps } from 'next/app';
 import '../styles/index.css';
-import 'animate.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-      <>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Component {...pageProps} />
-      </>
-    )
+    return <Component {...pageProps} />
 }
