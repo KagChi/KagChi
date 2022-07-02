0d63dc04f93f1a1c395cfb67a816d0f0800a049e
KagChi <yonuxbot@gmail.com>
2022-07-02 13:28:20 +0700

    feat: today progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import '../styles/index.css';
+import 'animate.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
