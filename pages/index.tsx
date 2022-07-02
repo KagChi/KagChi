3fc813924fce685726b8cec0f982314feb5cddcd
KagChi <59391215+KagChi@users.noreply.github.com>
2022-07-02 20:51:55 +0700

    fix: remove dark prefix

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,9 +110,9 @@ export default function render(): ReactNode {
               {!response && (
                 <>
                   <div className="flex items-center justify-center space-x-2">
-                    <div className="w-4 h-4 rounded-full animate-pulse dark:bg-yellow-400"></div>
-                    <div className="w-4 h-4 rounded-full animate-pulse dark:bg-yellow-400"></div>
-                    <div className="w-4 h-4 rounded-full animate-pulse dark:bg-yellow-400"></div>
+                    <div className="w-4 h-4 rounded-full animate-pulse bg-yellow-400"></div>
+                    <div className="w-4 h-4 rounded-full animate-pulse bg-yellow-400"></div>
+                    <div className="w-4 h-4 rounded-full animate-pulse bg-yellow-400"></div>
                   </div>
                 </>
               )}
