1076481f9929ec64143f77ea89787ce2f28569ac
KagChi <yonuxbot@gmail.com>
2022-07-02 15:22:54 +0700

    feat: today progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <ParticlesComponent />
       <BackGroundComponent />
-      <div className="animate__animated animate__bounceInDown animate__faster">
+      <div className="animate__animated animate__bounceInDown" style={{ "animationDuration": "200ms" }}>
         <header className="p-4">
           <div className="container flex justify-center h-16 mx-auto">
             <ul className="items-stretch space-x-4 flex">
@@ -32,7 +32,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                 <button
                   onClick={() => router.push("/about")}
                   className={`flex items-center px-4 -mb-1 font-segoe-bold border-b-2 ${
-                    router.pathname === "/about" ? "text-yellow-700 border-yellow-700" : inactiveCSS
+                    router.pathname === "/about"
+                      ? "text-yellow-700 border-yellow-700"
+                      : inactiveCSS
                   }`}
                 >
                   About
@@ -42,6 +44,73 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           </div>
         </header>
         <Component {...pageProps} />
+
+        <div className="relative mt-16 bg-yellow-700">
+          <svg
+            className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-yellow-700"
+            preserveAspectRatio="none"
+            viewBox="0 0 1440 54"
+          >
+            <path
+              fill="currentColor"
+              d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
+            />
+          </svg>
+          <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+            <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+              <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+                <div>
+                  <p className="font-semibold tracking-wide font-segoe-bold">
+                    Navigation
+                  </p>
+                  <ul className="mt-2 space-y-2">
+                    <li>
+                      <a
+                        href="https://tunebot.org"
+                        className="transition-colors duration-300 font-segoe"
+                      >
+                        Tune
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://nezu.my.id"
+                        className="transition-colors duration-300 font-segoe"
+                      >
+                        Nezu
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/tanjiro-project/kamado-tanjiro"
+                        className="transition-colors duration-300 font-segoe"
+                      >
+                        Tanjiro
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between pt-5 pb-10 border-t border-black sm:flex-row">
+              <p className="text-sm font-segoe-bold">
+                © Copyright 2022 KagChi All rights reserved.
+              </p>
+              <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+                <a href="https://github.com/KagChi/KagChi" className="transition-colors duration-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
 export default function render(): ReactNode {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -33,6 +35,108 @@ export default function render(): ReactNode {
           </div>
         </div>
       </div>
+
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-10 lg:grid-cols-2">
+          <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+              <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+                <polyline
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  points=" 8,5 8,1 16,1 16,5"
+                  strokeLinejoin="round"
+                />
+                <polyline
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  points="9,15 1,15 1,5 23,5 23,15 15,15"
+                  strokeLinejoin="round"
+                />
+                <polyline
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  points="22,18 22,23 2,23 2,18"
+                  strokeLinejoin="round"
+                />
+                <rect
+                  x="9"
+                  y="13"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  width="6"
+                  height="4"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="max-w-xl mb-6">
+              <h2 className="max-w-lg mb-6 text-3xl font-segoe-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                Become a {" "} <span className="inline-block text-yellow-700">
+                  developer
+                </span>
+                {" "}
+                among them
+              </h2>
+              <p className="text-base text-gray-700 font-segoe md:text-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae. explicabo.
+              </p>
+            </div>
+            <div>
+              <button
+                onClick={() => router.push("/about")}
+                className="inline-flex items-center font-segoe-bold transition-colors duration-200 text-yellow-700 hover:text-yellow-800"
+              >
+                Learn more
+                <svg
+                  className="inline-block w-3 ml-2"
+                  fill="currentColor"
+                  viewBox="0 0 12 12"
+                >
+                  <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center justify-center -mx-4 lg:pl-8">
+            <div className="flex flex-col items-end px-3">
+              <img
+                className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+                src="https://cdn.discordapp.com/avatars/546627382153576455/5ee994bb1703e95f90cc03851a479ac0.png?size=2048"
+                alt="Tanjiro San"
+              />
+              <img
+                className="object-cover w-20 h-20 rounded-xl shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+                src="https://cdn.discordapp.com/avatars/616169470293049344/8816a7d1410bf85998c36775b2ba9990.png?size=2048"
+                alt="NezukoChan"
+              />
+            </div>
+            <div className="px-3">
+              <img
+                className="object-cover w-40 h-40 rounded-xl shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+                src="https://cdn.discordapp.com/avatars/489076647727857685/a20d6d7baab44ad66b4470f2e0a016aa.png?size=2048"
+                alt="Musical Tune"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
     </>
   );
 }
