2ba8c6b3484b1bb0adae78f1ba36f69547e4318f
KagChi <yonuxbot@gmail.com>
2024-01-16 11:40:09 +0700

    feat: add metadata

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,16 @@ import "@/app.css";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Suspense } from "solid-js";
+import { Title, Meta, MetaProvider } from "@solidjs/meta";
 
 export default function App() {
     return (
-        <Router root={(props) => <Suspense>{props.children}</Suspense>}>
-            <FileRoutes />
-        </Router>
+        <>
+            <MetaProvider>
+                <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+                    <FileRoutes />
+                </Router>
+            </MetaProvider>
+        </>
     );
 }
