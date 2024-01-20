fb20b521489dd9e238fd875450e76554d41633fe
KagChi <59391215+KagChi@users.noreply.github.com>
2024-01-20 12:40:14 +0700

    fix: remove debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "@solidjs/start/config";
-
 import { resolve } from "path";
-
-console.log(resolve(__dirname, "src"));
 
 export default defineConfig({
     start: { ssr: false },
