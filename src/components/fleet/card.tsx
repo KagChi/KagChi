ad98da86ff31a8e1c67e51bfdeffcd2ccbe4728a
KagChi <yonuxbot@gmail.com>
2024-06-30 23:04:03 +0700

    fix: update border

--- src/components/fleet/card.tsx
+++ src/components/fleet/card.tsx
@@ -7,9 +7,9 @@ export function Card({ image, website, name, description, github }: Project) {
     return (
         <div className="relative block cursor-pointer h-full group">
             <div className="absolute inset-0 rounded-md border-2 border-dashed border-orange-200 transition-transform transform opacity-0 group-hover:opacity-100 pointer-events-none" />
-            <div className="flex flex-col h-full rounded-md mb-4 transition-transform transform group-hover:-translate-x-2 group-hover:-translate-y-2 bg-black">
+            <div className="border border-gray-900 flex flex-col h-full rounded-md mb-4 transition-transform transform group-hover:-translate-x-2 group-hover:-translate-y-2 bg-black">
                 <div className="w-full h-28 bg-cover rounded-t-md" style={{ backgroundImage: `url(${image})` }} />
-                <div className="border border-gray-900 rounded-b-md py-2 px-4 flex-grow flex flex-col gap-4">
+                <div className="rounded-b-md py-2 px-4 flex-grow flex flex-col gap-4">
                     <div>
                         <p className="font-bold">{name}</p>
                         <p className="text-xs text-gray-500">{description}</p>
